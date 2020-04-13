25e6d8985e1b2835d4f5d6f4eba122ddfb8575c6
Hassane Sow <imthassane@Gmail.com>
2020-04-13 02:11:01 +0200

    Changing geolocation api

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 The app just displays the current weather and some additional informations such as the temperature felt, the humidity, the pressure and the wind speed. <br />
 
-I'm using the https://ip-api.com api to get the current coordonates as the geolocation api has been disabled and the openweather api to get the current weather.
+I'm using the https://ipgeolocation.io api to get the current coordonates as the geolocation api has been disabled and the openweather api to get the current weather.
 
 It's a simple vanilla js website.
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // public key
 const API_KEY = "34383e7b10e62f60e2f9068e5e03cf5c";
-const icon = name => `http://openweathermap.org/img/wn/${name}@2x.png`;
+const GEO_KEY = "ce0038a860f0480ea54ef733eaf7a06c";
+
+const icon = name => `https://openweathermap.org/img/wn/${name}@2x.png`;
 
 let _celsius =          document.querySelector("#celsius"),
     _farenheit =        document.querySelector("#farenheit"),
@@ -14,42 +16,45 @@ let _celsius =          document.querySelector("#celsius"),
     _icon =             document.querySelector("#icon");
 
 window.addEventListener('load', async () => {
-    // Fetching the coordonates.
-    let response = await fetch("http://ip-api.com/json/");
-    let ipData = await response.json();
+    try {
+        // Fetching the coordonates.
+        let response = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${GEO_KEY}`);
+        let ipData = await response.json();
 
-    // Getting the latitude and longitude.
-    const { lat, lon } = ipData;
+        // Getting the latitude and longitude.
+        const { latitude, longitude } = ipData;
 
-    // Fetching the meteo.
-    response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`);
-    let data = await response.json();
+        // Fetching the meteo.
+        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`);
+        let data = await response.json();
 
-    const degree = parseInt(data.main.temp);
-    const degreeFarenheit = parseInt(convertCtoF(degree));
+        const degree = parseInt(data.main.temp);
+        const degreeFarenheit = parseInt(convertCtoF(degree));
 
-    const flCelsius = parseInt(data.main.feels_like);
-    const flFarenheit = parseInt(convertCtoF(flCelsius));
+        const flCelsius = parseInt(data.main.feels_like);
+        const flFarenheit = parseInt(convertCtoF(flCelsius));
 
-    const iconName = data.weather[0].icon;
-    const windSpeed = parseInt(data.wind.speed);
-    const pressure = parseInt(data.main.pressure);
-    const humidity = parseInt(data.main.humidity);
+        const iconName = data.weather[0].icon;
+        const windSpeed = parseInt(data.wind.speed);
+        const pressure = parseInt(data.main.pressure);
+        const humidity = parseInt(data.main.humidity);
 
-    // Updating the user interface.
-    _locationTimezone.textContent = ipData.timezone;
-    _city.textContent = ipData.city;
+        // Updating the user interface.
+        _locationTimezone.textContent = ipData.time_zone.name;
+        _city.textContent = ipData.city;
 
-    _celsius.textContent = `${degree} °c`;
-    _farenheit.textContent = `${degreeFarenheit} °f`;
+        _celsius.textContent = `${degree} °c`;
+        _farenheit.textContent = `${degreeFarenheit} °f`;
 
-    _flCelsius.textContent = `${flCelsius} °c`;
-    _flFarenheit.textContent = `${flFarenheit} °f`;
-    _windSpeed.textContent = `${windSpeed} mps`;
-    _humidity.textContent = `${humidity}%`;
-    _pressure.textContent = `${pressure} hpa`;
-    _icon.innerHTML = `<img src="${icon(iconName)}" />`;
-
+        _flCelsius.textContent = `${flCelsius} °c`;
+        _flFarenheit.textContent = `${flFarenheit} °f`;
+        _windSpeed.textContent = `${windSpeed} mps`;
+        _humidity.textContent = `${humidity}%`;
+        _pressure.textContent = `${pressure} hpa`;
+        _icon.innerHTML = `<img src="${icon(iconName)}" />`;
+    } catch(ex) {
+        console.log(ex.message);
+    }
 });
 
 const convertCtoF = temp => temp * 5/9 + 32;
